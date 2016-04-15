590491a76278be8ec7c127d0a939d18a5dfa72d6
Gareth Lewin <gareth@garethlewin.com>
2016-04-15 12:25:25 -0700

    Change the statsd stats object to take a client as a parameter
    Summary:
    The original version of this created the cactus/go-statsd-client client inside the object, but
    it's more likely that users of kinsumer will already have their own statsd object.
    
    Additionally had to change the noopkinsumer example
    
    Test Plan: go test ./... but also being tested in spade
    
    Reviewers: mperrone, spencer
    
    Reviewed By: spencer
    
    Subscribers: jackgao, rhu, maasted
    
    Differential Revision: https://phabricator.internal.justin.tv/D3253

--- statsd/statsd.go
+++ statsd/statsd.go
@@ -10,39 +10,46 @@ import (
 
 // Statsd is a statreceiver that writes stats to a statsd endpoint
 type Statsd struct {
-	Client statsd.Statter
+	client statsd.Statter
 }
 
-// New creates a new Statsd statreceiver
+// New creates a new Statsd statreceiver with a new instance of a cactus statter
 func New(addr, prefix string) (*Statsd, error) {
 	sd, err := statsd.NewClient(addr, prefix)
 	if err != nil {
 		return nil, err
 	}
 	return &Statsd{
-		Client: sd,
+		client: sd,
 	}, nil
+}
+
+// NewWithStatter creates a new statreciever wrapping an existing statter
+func NewWithStatter(client statsd.Statter) *Statsd {
+	return &Statsd{
+		client: client,
+	}
 }
 
 // Checkpoint implementation that writes to statsd
 func (s *Statsd) Checkpoint() {
-	_ = s.Client.Inc("kinsumer.checkpoints", 1, 1.0)
+	_ = s.client.Inc("kinsumer.checkpoints", 1, 1.0)
 }
 
 // EventToClient implementation that writes to statsd metrics about a record
-// that was consumed by the Client
+// that was consumed by the client
 func (s *Statsd) EventToClient(inserted, retrieved time.Time) {
 	now := time.Now()
 
-	_ = s.Client.Inc("kinsumer.consumed", 1, 1.0)
-	_ = s.Client.TimingDuration("kinsumer.in_stream", retrieved.Sub(inserted), 1.0)
-	_ = s.Client.TimingDuration("kinsumer.end_to_end", now.Sub(inserted), 1.0)
-	_ = s.Client.TimingDuration("kinsumer.in_kinsumer", now.Sub(retrieved), 1.0)
+	_ = s.client.Inc("kinsumer.consumed", 1, 1.0)
+	_ = s.client.TimingDuration("kinsumer.in_stream", retrieved.Sub(inserted), 1.0)
+	_ = s.client.TimingDuration("kinsumer.end_to_end", now.Sub(inserted), 1.0)
+	_ = s.client.TimingDuration("kinsumer.in_kinsumer", now.Sub(retrieved), 1.0)
 }
 
 // EventsFromKinesis implementation that writes to statsd metrics about records that
 // were retrieved from kinesis
 func (s *Statsd) EventsFromKinesis(num int, lag time.Duration) {
-	_ = s.Client.TimingDuration("kinsumer.lag", lag, 1.0)
-	_ = s.Client.Inc("kinsumer.retrieved", int64(num), 1.0)
+	_ = s.client.TimingDuration("kinsumer.lag", lag, 1.0)
+	_ = s.client.Inc("kinsumer.retrieved", int64(num), 1.0)
 }
