fab97f442abe73aeb55baada0524768b76c4cd6c
Emile Fugulin <emilefugulin@hotmail.com>
2019-06-28 11:06:04 -0700

    Add missing comments

--- logger.go
+++ logger.go
@@ -2,12 +2,16 @@ package kinsumer
 
 import "log"
 
+// Logger is a minimal interface to allow custom loggers to be used
 type Logger interface {
 	Log(string, ...interface{})
 }
 
+// DefaultLogger is a logger that will log using the
+// standard golang log library
 type DefaultLogger struct{}
 
+// Log implementation that uses golang log library
 func (*DefaultLogger) Log(format string, v ...interface{}) {
-	log.Printf(format, v)
+	log.Printf(format, v...)
 }
