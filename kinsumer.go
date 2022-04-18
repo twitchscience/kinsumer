f4f019fb7435a87b79ce123a4e8555d5c37fa53a
Mark Robinson <mrobinson@plaid.com>
2022-04-17 20:24:40 -0700

    Reset to latest

--- kinsumer.go
+++ kinsumer.go
@@ -118,7 +118,7 @@ func NewWithInterfaces(kinesis kinesisiface.KinesisAPI, dynamodb dynamodbiface.D
 	return consumer, nil
 }
 
-func (k *Kinsumer) ResetToPresent() error {
+func (k *Kinsumer) ResetToLatest() error {
 
 	shardIDs, err := loadShardIDsFromKinesis(k.kinesis, k.streamName)
 	if err != nil {
